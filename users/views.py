a45a29c6175fa9c0ff1512230e72e6cdd6e7020d
kazila <mrkazzila@gmail.com>
2023-01-13 00:37:04 +0300

    update info about count items in basket and items price

--- users/views.py
+++ users/views.py
@@ -47,16 +47,10 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user)
 
-    baskets = Basket.objects.filter(user=request.user)
-    total_sum = sum(basket.sum for basket in baskets)
-    total_quantity = sum(basket.quantity for basket in baskets)
-
     context = {
         'title': 'Store - Профиль',
         "form": form,
-        "baskets": baskets,
-        'total_sum': total_sum,
-        'total_quantity': total_quantity,
+        "baskets": Basket.objects.filter(user=request.user),
     }
     return render(request=request, template_name='users/profile.html', context=context)
 
