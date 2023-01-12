874c4d2816a100be24f7109db86dd8f29f9eacf2
kazila <mrkazzila@gmail.com>
2023-01-12 20:03:55 +0300

    add users app

---
+++ users/views.py
@@ -0,0 +1,54 @@
+from django.contrib import auth, messages
+from django.shortcuts import render, HttpResponseRedirect
+# Create your views here.
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserLoginForm()
+    context = {"form": form}
+    return render(request=request, template_name='users/login.html', context=context)
+
+
+def registration(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Поздравляем! Вы успешно зарегистрировались!")
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request=request, template_name='users/registration.html', context=context)
+
+
+def profile(request):
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', "form": form}
+    return render(request=request, template_name='users/profile.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
