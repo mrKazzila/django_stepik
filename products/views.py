8191a78c473ab1a8f2ddc4338ae426bea7ed3f64
kazila <mrkazzila@gmail.com>
2023-01-16 21:26:35 +0300

    Add comment for view

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from store.settings import LOGIN_URL
 from django.core.paginator import Paginator
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 
 class IndexView(TemplateView):
@@ -38,20 +39,21 @@ class ProductsListView(ListView):
         return queryset.filter(category__id=category_id) if category_id is not None else queryset
 
 
-
-# def products(request, category_id=None, page_number=1):
-#     products = Product.objects.filter(category__id=category_id) if category_id else Product.objects.all()
-#     per_page = 3
-#     paginator = Paginator(products, per_page)
-#     products_paginator = paginator.page(page_number)
-#     context = {
-#         'title': 'Store - Каталог',
-#         'categories': ProductCategory.objects.all(),
-#         'products': products_paginator,
-#     }
-#     return render(request=request, template_name='products/products.html', context=context)
-
-
+# class BasketCreateView(CreateView):
+#     model = Basket
+#
+#     def post(self, request, *args, **kwargs):
+#         product = Product.objects.get(id=self.kwargs.get("product_id"))
+#         baskets = Basket.objects.filter(user=request.user, product=product)
+#
+#         if not baskets.exists():
+#             Basket.objects.create(user=request.user, product=product, quantity=1)
+#         else:
+#             basket = Basket.objects.first()
+#             basket.quantity += 1
+#             basket.save()
+#
+#         return HttpResponseRedirect(request.META['HTTP_REFERER'])
 @login_required(login_url=LOGIN_URL)
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
