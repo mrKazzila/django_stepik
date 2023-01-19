7f9fa492b79bbf27a49b72ae2a52b3c04e0b4bbd
kazila <mrkazzila@gmail.com>
2023-01-20 00:47:43 +0300

    return fix 5

--- store/settings.py
+++ store/settings.py
@@ -211,7 +211,7 @@ if DEBUG:
     )
 else:
     # STATIC_ROOT = BASE_DIR / 'static'  # for prod
-    STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
