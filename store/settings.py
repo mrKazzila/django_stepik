69c67f3f60806ef645a1075b7d6e271562aef85b
kazila <mrkazzila@gmail.com>
2023-01-19 22:59:21 +0300

    fix bug with STATIC_ROOT

--- store/settings.py
+++ store/settings.py
@@ -204,12 +204,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-        BASE_DIR / 'static',
-)
 
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = BASE_DIR / 'static'  # for prod
+else:
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
