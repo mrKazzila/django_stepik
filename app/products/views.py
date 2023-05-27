031a4a4bcd1497ee1563cca6832c658f556dbd8e
mrKazzila <mrkazzila@gmail.com>
2023-05-27 10:45:33 +0300

    refactor: update slides views & template

--- app/products/views.py
+++ app/products/views.py
@@ -18,12 +18,13 @@ class ProductsListView(TitleMixin, ListView):
     model = Product
     template_name = 'products/products.html'
     paginate_by = 3
-
     title = 'Store - Catalog'
+    slide_range = range(1, 6)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
         context['categories'] = ProductCategory.objects.all()
+        context['slide_range'] = self.slide_range
         return context
 
     def get_queryset(self):
