fcf8fd32ef6285467d6385e71a3e40737dd1ca2f
mrKazzila <mrkazzila@gmail.com>
2023-05-27 18:52:48 +0300

    style: fix style errors

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -5,11 +5,16 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    """Register the Order model and settings fields for admin"""
+
     list_display = ('__str__', 'status')
     fields = (
-        'id', 'created',
+        'id',
+        'created',
         ('first_name', 'last_name'),
         ('email', 'address'),
-        'basket_history', 'status', 'initiator',
+        'basket_history',
+        'status',
+        'initiator',
     )
     readonly_fields = ('id', 'created')
