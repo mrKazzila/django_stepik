1e8ab227342d1ca4fd9a2b9a9941742f0bacb4aa
mrKazzila <mrkazzila@gmail.com>
2023-05-28 18:50:20 +0300

    feat: add broker url

--- app/config/celery.py
+++ app/config/celery.py
@@ -1,11 +1,13 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('proj')
+app = Celery('service')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 
