5508615b7a8c57715142f496a40126128d4f35f8
mrKazzila <mrkazzila@gmail.com>
2023-05-29 19:00:56 +0300

    chore: update imports

--- app/config/settings/__init__.py
+++ app/config/settings/__init__.py
@@ -1,8 +1,13 @@
-from .auth import *  # noqa: F401, F403
-from .celery import *  # noqa: F401, F403
+from .auth import (  # noqa: F401, F403
+    AUTHENTICATION_BACKENDS,
+    AUTH_PASSWORD_VALIDATORS,
+    AUTH_USER_MODEL,
+    SOCIALACCOUNT_PROVIDERS,
+)
+from .celery import CELERY_BROKER_URL, CELERY_RESULT_BACKEND  # noqa: F401, F403
 from .common import *  # noqa: F401, F403
-from .database import *  # noqa: F401, F403
+from .database import DATABASES  # noqa: F401, F403
 from .email import *  # noqa: F401, F403
-from .logging import *  # noqa: F401, F403
-from .redis import *  # noqa: F401, F403
-from .stripe import *  # noqa: F401, F403
+from .logging import LOGGING  # noqa: F401, F403
+from .redis import CACHES, REDIS_HOST, REDIS_PORT  # noqa: F401, F403
+from .stripe import STRIPE_PUBLIC_KEY, STRIPE_SECRET_KEY, STRIPE_WEBHOOK_SECRET  # noqa: F401, F403
