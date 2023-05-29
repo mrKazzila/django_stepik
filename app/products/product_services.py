b7c944f18ede7dc262876d4cde83a40002cf5cd1
mrKazzila <mrkazzila@gmail.com>
2023-05-29 11:05:01 +0300

    chore: add logger

--- app/products/product_services.py
+++ app/products/product_services.py
@@ -1,10 +1,17 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def update_or_add_to_basket(user, basket_obj, product_obj, basket_model):
     if not basket_obj.exists():
         basket_model.objects.create(user=user, product=product_obj, quantity=1)
+        logger.debug('Create basket obj')
     else:
         basket = basket_model.objects.first()
         basket.quantity += 1
         basket.save()
+        logger.debug('Update basket obj')
 
 
 def create_basket_item_json(product_name, quantity, price, _sum):
