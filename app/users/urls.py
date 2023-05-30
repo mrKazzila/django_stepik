7ee42c38f6d543b875baaf93137571436f82862b
mrKazzila <mrkazzila@gmail.com>
2023-05-30 12:20:48 +0300

    style: yapf formatter

--- app/users/urls.py
+++ app/users/urls.py
@@ -2,8 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import (EmailVerificationView, UserLoginView,
-                    UserProfileUpdateView, UserRegistrationCreateView)
+from .views import (EmailVerificationView, UserLoginView, UserProfileUpdateView, UserRegistrationCreateView)
 
 app_name = 'users'
 
@@ -13,5 +12,4 @@ urlpatterns = [
     path('profile/<int:pk>', login_required(UserProfileUpdateView.as_view()), name='profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('ver/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification'),
-
 ]
