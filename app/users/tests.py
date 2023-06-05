84cec7fef001491b16bc940da163765e4f8b874a
mrKazzila <mrkazzila@gmail.com>
2023-06-05 13:49:27 +0300

    chore: update tests

--- app/users/tests.py
+++ app/users/tests.py
@@ -1,11 +1,10 @@
-from datetime import timedelta
 from http import HTTPStatus
 
 from django.test import TestCase
 from django.urls import reverse
-from django.utils.timezone import now
 
-from .models import EmailVerification, User
+from .models import User
+from .forms import UserLoginForm
 
 
 class UserRegistrationViewTestCase(TestCase):
@@ -21,34 +20,60 @@ class UserRegistrationViewTestCase(TestCase):
         }
         self.path = reverse('users:registration')
 
-    def __common_tests(self, response):
+    def _common_tests(self, response):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Registration')
         self.assertTemplateUsed(response, 'users/registration.html')
 
     def test_user_registration_get(self):
         response = self.client.get(self.path)
-
-        self.__common_tests(response=response)
+        self._common_tests(response=response)
 
     def test_user_registration_post_success(self):
         username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
 
         response = self.client.post(self.path, self.data)
 
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertTrue(User.objects.filter(username=username).exists())
 
-        email_verification = EmailVerification.objects.filter(user__username=username)
-        self.assertTrue(email_verification.exists())
-        self.assertEqual(
-            email_verification.first().expiration.date(),
-            (now() + timedelta(hours=48)).date(),
-        )
-
     def test_user_registration_post_error(self):
         User.objects.create(username=self.data['username'])
         response = self.client.post(self.path, self.data)
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(response, 'A user with this name already exists.', html=True)
+        self.assertContains(response, 'A user with that username already exists.', html=True)
+
+
+class UserLoginViewTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword',
+        )
+        self.login_url = reverse('users:login')
+
+    def test_login_page_loads_successfully(self):
+        response = self.client.get(self.login_url)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertIsInstance(response.context['form'], UserLoginForm)
+
+    def test_login_successful(self):
+        _data = {'username': 'testuser', 'password': 'testpassword'}
+        response = self.client.post(self.login_url, _data)
+
+        self.assertRedirects(response, reverse('index'))
+
+    def test_login_failure(self):
+        _data = {'username': 'testuser', 'password': 'wrongpassword'}
+        response = self.client.post(self.login_url, _data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertIn('form', response.context)
+        self.assertTrue(response.context['form'].errors)
