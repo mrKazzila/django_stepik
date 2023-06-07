09496b30fb91607dca32adf5fd83126dddbc13ff
mrKazzila <mrkazzila@gmail.com>
2023-06-07 14:11:40 +0300

    fix: fix STATIC_ROOT settings

--- app/config/settings/common.py
+++ app/config/settings/common.py
@@ -94,12 +94,9 @@ USE_TZ = True
 
 # Static files
 
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = (BASE_DIR / '../static',)
-else:
-    STATIC_URL = '/staticfiles/'
-    STATIC_ROOT = BASE_DIR / '../staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR / '../static',)
+STATIC_ROOT = BASE_DIR / '../staticfiles'
 
 # Media files
 
